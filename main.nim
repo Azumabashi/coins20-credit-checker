7ddd79ac857ec1317cf83aa25a95ac2710717385
Azumabashi <>
2022-09-28 22:36:10 +0900

    update creditConditions

--- main.nim
+++ main.nim
@@ -36,7 +36,169 @@ const creditConditions: seq[CreditCondition] = @[
         cond: "(ソフトウェアサイエンス|情報システム|知能情報メディア)実験A",
         required: 3,
         matchType: MatchType.CourseName
-    )
+    ),
+    CreditCondition(
+        title: "主専攻実験B",
+        cond: "(ソフトウェアサイエンス|情報システム|知能情報メディア)実験B",
+        required: 3,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "卒業研究A",
+        cond: "卒業研究A",
+        required: 3,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "卒業研究B",
+        cond: "卒業研究B",
+        required: 3,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "専門語学A",
+        cond: "専門語学A",
+        required: 3,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "専門語学B",
+        cond: "専門語学B",
+        required: 3,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "GBx0",
+        cond: "GB(2|3|4)0\\d{3}",
+        required: 18,
+        matchType: MatchType.CourseCode
+    ),
+    CreditCondition(
+        title: "GB(2|3|4)",
+        cond: "GB(2|3|4)[1-9]\\d{3}",
+        required: 0,
+        matchType: MatchType.CourseCode
+    ),
+    CreditCondition(
+        title: "情報特別演習",
+        cond: "(情報科学特別演習|情報特別演習I|情報特別演習II)",
+        required: 0,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "線形代数A",
+        cond: "線形代数A",
+        required: 2,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "線形代数B",
+        cond: "線形代数B",
+        required: 2,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "微分積分A",
+        cond: "微分積分A",
+        required: 2,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "微分積分B",
+        cond: "微分積分B",
+        required: 2,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "情報数学A",
+        cond: "情報数学A",
+        required: 2,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "専門英語基礎",
+        cond: "専門英語基礎",
+        required: 1,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "プログラミング入門",
+        cond: "プログラミング入門",
+        required: 3,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "コンピュータとプログラミング",
+        cond: "コンピュータとプログラミング",
+        required: 3,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "データ構造とアルゴリズム",
+        cond: "データ構造とアルゴリズム",
+        required: 3,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "データ構造とアルゴリズム実験",
+        cond: "データ構造とアルゴリズム実験",
+        required: 2,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "論理回路",
+        cond: "論理回路",
+        required: 2,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "論理回路演習",
+        cond: "論理回路演習",
+        required: 2,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "確率・統計等",
+        cond: "(確率論|統計学|数値計算法|論理と形式化|電磁気学|論理システム|論理システム演習)",
+        required: 10,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "Computer Science in English",
+        cond: "Computer Science in English (A|B)",
+        required: 2,
+        matchType: MatchType.CourseName
+    ),
+    CreditCondition(
+        title: "GB1",
+        cond: "GB1\\d{4}",
+        required: 0,
+        matchType: MatchType. CourseCode
+    ),
+    CreditCondition(
+        title: "GA1",
+        cond: "GA1\\d{4}",
+        required: 8,
+        matchType: MatchType. CourseCode
+    ),
+    CreditCondition(
+        title: "フレッシュマン・セミナー",
+        cond: "フレッシュマン・セミナー",
+        required: 1,
+        matchType: MatchType. CourseName
+    ),
+    CreditCondition(
+        title: "学問への誘い",
+        cond: "学問への誘い",
+        required: 1,
+        matchType: MatchType. CourseName
+    ),
+    CreditCondition(
+        title: "体育",
+        cond: ".*体育.*",
+        required: 1,
+        matchType: MatchType. CourseName
+    ),
 ]
 
 proc str2score(s: string): Score = 
