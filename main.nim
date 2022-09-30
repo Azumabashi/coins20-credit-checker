67a836348ba00ec26066d9a6fed264b18ddd37d4
Azumabashi <>
2022-09-30 19:44:25 +0900

    add property to creditConditions

--- main.nim
+++ main.nim
@@ -19,238 +19,272 @@ var creditConditions: seq[CreditCondition] = @[
         cond: "(ソフトウェアサイエンス|情報システム|知能情報メディア)実験A",
         required: 3,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyRequired
+        courseType: CourseType.SpecialtyRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "主専攻実験B",
         cond: "(ソフトウェアサイエンス|情報システム|知能情報メディア)実験B",
         required: 3,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyRequired
+        courseType: CourseType.SpecialtyRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "卒業研究A",
         cond: "卒業研究A",
         required: 3,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyRequired
+        courseType: CourseType.SpecialtyRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "卒業研究B",
         cond: "卒業研究B",
         required: 3,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyRequired
+        courseType: CourseType.SpecialtyRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "専門語学A",
         cond: "専門語学A",
         required: 3,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyRequired
+        courseType: CourseType.SpecialtyRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "専門語学B",
         cond: "専門語学B",
         required: 3,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyRequired
+        courseType: CourseType.SpecialtyRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "GBx0",
         cond: "GB(2|3|4)0\\d{3}",
         required: 18,
         matchType: MatchType.CourseCode,
-        courseType: CourseType.SpecialtyElective
+        courseType: CourseType.SpecialtyElective,
+        acquired: false
     ),
     CreditCondition(
         title: "GB(2|3|4)",
         cond: "GB(2|3|4)[1-9]\\d{3}",
         required: 0,
         matchType: MatchType.CourseCode,
-        courseType: CourseType.SpecialtyElective
+        courseType: CourseType.SpecialtyElective,
+        acquired: false
     ),
     CreditCondition(
         title: "情報特別演習",
         cond: "(情報科学特別演習|情報特別演習I|情報特別演習II)",
         required: 0,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyElective
+        courseType: CourseType.SpecialtyElective,
+        acquired: false
     ),
     CreditCondition(
         title: "線形代数A",
         cond: "線形代数A",
         required: 2,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "線形代数B",
         cond: "線形代数B",
         required: 2,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "微分積分A",
         cond: "微分積分A",
         required: 2,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "微分積分B",
         cond: "微分積分B",
         required: 2,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "情報数学A",
         cond: "情報数学A",
         required: 2,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "専門英語基礎",
         cond: "専門英語基礎",
         required: 1,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "プログラミング入門",
         cond: "プログラミング入門",
         required: 3,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "コンピュータとプログラミング",
         cond: "コンピュータとプログラミング",
         required: 3,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "データ構造とアルゴリズム",
         cond: "データ構造とアルゴリズム",
         required: 3,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "データ構造とアルゴリズム実験",
         cond: "データ構造とアルゴリズム実験",
         required: 2,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "論理回路",
         cond: "論理回路",
         required: 2,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "論理回路演習",
         cond: "論理回路演習",
         required: 2,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicRequired
+        courseType: CourseType.SpecialtyBasicRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "確率・統計等",
         cond: "(確率論|統計学|数値計算法|論理と形式化|電磁気学|論理システム|論理システム演習)",
         required: 10,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicElective
+        courseType: CourseType.SpecialtyBasicElective,
+        acquired: false
     ),
     CreditCondition(
         title: "Computer Science in English",
         cond: "Computer Science in English (A|B)",
         required: 2,
         matchType: MatchType.CourseName,
-        courseType: CourseType.SpecialtyBasicElective
+        courseType: CourseType.SpecialtyBasicElective,
+        acquired: false
     ),
     CreditCondition(
         title: "GB1",
         cond: "GB1\\d{4}",
         required: 0,
         matchType: MatchType. CourseCode,
-        courseType: CourseType.SpecialtyBasicElective
+        courseType: CourseType.SpecialtyBasicElective,
+        acquired: false
     ),
     CreditCondition(
         title: "GA1",
         cond: "GA1\\d{4}",
         required: 8,
         matchType: MatchType. CourseCode,
-        courseType: CourseType.SpecialtyBasicElective
+        courseType: CourseType.SpecialtyBasicElective,
+        acquired: false
     ),
     CreditCondition(
         title: "フレッシュマン・セミナー",
         cond: "フレッシュマン・セミナー",
         required: 1,
         matchType: MatchType. CourseName,
-        courseType: CourseType.CommonRequired
+        courseType: CourseType.CommonRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "学問への誘い",
         cond: "学問への誘い",
         required: 1,
         matchType: MatchType. CourseName,
-        courseType: CourseType.CommonRequired
+        courseType: CourseType.CommonRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "体育",
         cond: "2\\d{6}",
         required: 2,
         matchType: MatchType. CourseCode,
-        courseType: CourseType.CommonRequired
+        courseType: CourseType.CommonRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "英語",
         cond: "31[HJKL].{4}",
         required: 4,
         matchType: MatchType. CourseCode,
-        courseType: CourseType.CommonRequired
+        courseType: CourseType.CommonRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "情報",
         cond: "6\\d{6}",
         required: 4,
         matchType: MatchType. CourseCode,
-        courseType: CourseType.CommonRequired
+        courseType: CourseType.CommonRequired,
+        acquired: false
     ),
     CreditCondition(
         title: "総合科目（学士基盤）",
         cond: "12\\d{5}",
         required: 1,
         matchType: MatchType. CourseCode,
-        courseType: CourseType.CommonElective
+        courseType: CourseType.CommonElective,
+        acquired: false
     ),
     CreditCondition(
         title: "体育・外国語・国語・芸術",
         cond: "\\d{7}",
         required: 0,
         matchType: MatchType. CourseCode,
-        courseType: CourseType.CommonElective
+        courseType: CourseType.CommonElective,
+        acquired: false
     ),
     CreditCondition(
         title: "文系科目",
         cond: "[A|B|C|D|V|W|Y].{6}",
         required: 6,
         matchType: MatchType. CourseCode,
-        courseType: CourseType.OtherElective
+        courseType: CourseType.OtherElective,
+        acquired: false
     ),
     CreditCondition(
         title: "理系科目",
         cond: "(E.{6}|F.{6}|GB\\d{5}|GC\\d{5}|H.{6})",
         required: 0,
         matchType: MatchType. CourseCode,
-        courseType: CourseType.OtherElective
+        courseType: CourseType.OtherElective,
+        acquired: false
     ),
 ]
 
